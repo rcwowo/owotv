a9ee9ec0ff764d2516c1721ff51bc9aace3fc98c
riley <riley@rcw.lol>
2026-03-30 11:42:46 -0400

    feat: Implemented pretty-feed-v3 RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -5,7 +5,8 @@ import { getCollection } from 'astro:content'
 
 export async function GET(context: APIContext) {
   try {
-    const vods = (await getCollection('vods'))
+    // Fetch the last 50 VODs from the collection
+    const vods = (await getCollection('vods')).splice(0, 50)
 
     // Sort posts by date
     const items = [...vods].sort(
@@ -18,12 +19,14 @@ export async function GET(context: APIContext) {
       title: SITE.TITLE,
       description: SITE.DESCRIPTION,
       site: context.site ?? SITE.SITEURL,
+      stylesheet: '/pretty-feed-v3.xsl',
       items: items.map((item) => ({
         title: item.data.Title,
-        categories: [item.data.Game],
-        description: `A stream that lasted ${item.data.Duration} while playing ${item.data.Game}.`,
-        pubDate: item.data['Stream Date'],
         link: `/watch/${item.id}/`,
+        pubDate: item.data['Stream Date'],
+        description: `A stream that lasted ${item.data.Duration} while playing ${item.data.Game}.`,
+        categories: [item.data.Game],
+        image: item.data['Thumbnail URL'],
       })),
     })
   } catch (error) {
